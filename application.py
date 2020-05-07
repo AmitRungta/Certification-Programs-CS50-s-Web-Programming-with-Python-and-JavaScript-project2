66190c1449ef84048b00419079ed8e9f03c14347
Amit Rungta <chehaktechnologies@gmail.com>
2020-05-07 12:43:10 +0530

    Basic files

---
+++ application.py
@@ -0,0 +1,149 @@
+import os
+import requests
+import time
+
+from flask import Flask, jsonify, render_template, request , redirect , url_for
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+socketio = SocketIO(app)
+
+
+
+
+class UserData:
+    """class to save user name and his registered channels. """
+    def __init__(self, DisplayName):
+        self.DisplayName = DisplayName
+        self.ChannelList = []
+
+    def RegisterChannel(self, ChhanelName):
+        if ( ( None == ChhanelName ) or (not(ChhanelName and ChhanelName.strip())) ) :
+            return "Channel name is not valid"
+        
+        if ( ChhanelName in self.ChannelList ):
+            return "Already registered with this channel"
+
+        self.ChannelList.append (ChhanelName)
+
+        return True
+
+
+class PostData:       
+    """class to store individual post data. """
+    def __init__(self, UserName , PostDate , PostString,PostID):
+        self.UserName = UserName
+        self.PostDate = PostDate
+        self.PostString = PostString
+        self.PostID = PostID
+
+
+class Channeldata:       
+    """class to store individual channel data. """
+    def __init__(self, ChannelName , ChannelGenre):
+        self.ChannelName = ChannelName
+        self.ChannelGenre = ChannelGenre
+
+
+# list of selected users.
+listOfUsers =[]
+
+# list of channels.
+listOfChannels =[]
+
+
+@app.route("/")
+def index():
+    """ Default page to retun login data """
+    return render_template("index.html")
+
+
+@app.route("/AddChannel", methods = ['GET', 'POST'])
+def AddChannel():
+
+    if request.method == 'GET':
+        # if the user have come here directly then move back to index page.
+        return render_template("addchannel.html")
+
+    channelName = request.form.get('ChannelName' , "")
+    channelGenre = request.form.get('ChannelGenre' , "")
+
+    if ( ( None == channelName ) or (not(channelName and channelName.strip())) ) :
+        return jsonify({"success": False , "reason": "Channel name is empty"})
+    if ( ( None == channelGenre ) or (not(channelGenre and channelGenre.strip())) ) :
+        return jsonify({"success": False , "reason": "Channel genere is empty"})
+
+    channelName = channelName.strip()
+    channelGenre = channelGenre.strip()
+
+    AlreadyPresent = any ( x for x in listOfChannels if x.ChannelName.lower() == channelName.lower())
+    if True == AlreadyPresent :
+        return jsonify({"success": False , "reason": "Channel with given name already present"})
+
+    listOfChannels.append ( Channeldata(channelName,channelGenre))
+
+    # emit("Channel List Updated", listOfChannels, broadcast=True)
+    return jsonify({"success": True })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# AmitTempCode TO delete here....
+votes = {"yes": 0, "no": 0, "maybe": 0}
+
+@app.route("/postpage")
+def postpage():
+    return render_template("UserPost.html")
+
+
+@socketio.on("submit vote")
+def vote(data):
+    if data:
+        selection = data["selection"]
+        votes[selection] += 1
+        emit("vote totals", votes, broadcast=True)
+
+
+
+
+@app.route("/posts", methods=["POST"])
+def posts():
+
+    # Get start and end point for posts to generate.
+    start = int(request.form.get("start") or 0)
+    end = int(request.form.get("end") or (start + 9))
+
+    # Generate list of posts.
+    data = []
+    for i in range(start, end + 1):
+        data.append(f"Post #{i}")
+
+    # Artificially delay speed of response.
+    time.sleep(1)
+
+    # Return list of posts.
+    return jsonify(data)
---
+++ static/index.js
@@ -0,0 +1,142 @@
+const LoggedUserKey = 'LoggedUserName' ;
+
+
+function fnGetCurUserName ()
+{
+    let LoggedUserName = "";
+    if (!localStorage.getItem(LoggedUserKey))
+        localStorage.setItem(LoggedUserKey, LoggedUserName);
+    else 
+        LoggedUserName = localStorage.getItem(LoggedUserKey );
+
+    LoggedUserName = LoggedUserName.trim() ;
+    return LoggedUserName ;
+}
+
+
+function fnLoadUserPage( )
+{
+    // Lets first check if any user is checked or not.
+    const LoggedUserName = fnGetCurUserName () ;
+    if ( LoggedUserName === "" )
+        return false ;
+
+    window.open ( "/AddChannel" , "_top");
+    return true ;
+}
+
+
+// Login button from login form.    
+function fnLogin(event)
+{
+    retNode = document.querySelector('#inputUserName') ;
+    if ( null === retNode)
+        return false ;
+    LoggedUserName = retNode.value;
+    LoggedUserName = LoggedUserName.trim() ;
+    if ( LoggedUserName === "" )
+    {
+        alert(`Username cannot be left empty!!!`);
+        return false ;
+    }         
+    localStorage.setItem(LoggedUserKey, LoggedUserName);
+    event.preventDefault()
+    fnLoadUserPage( );
+}
+
+
+// Logout from current user.    
+function fnLogout(event)
+{
+    localStorage.setItem(LoggedUserKey, "");
+    window.open ( "/" , "_top");
+    event.preventDefault()
+}
+
+
+
+
+// Add a new channel button from add channel form.    
+function fnAddChannel(event) {
+
+    event.preventDefault()
+    
+    let UsrErrMsgNode = document.querySelector('#UserErrMsg') ; 
+    UsrErrMsgNode.style.display = "none";
+
+    ChannelNameNode = document.querySelector('#inputChannelName');
+    ChannelGenreNode = document.querySelector('#inputChannelGenre');
+    if (null === ChannelNameNode || null === ChannelGenreNode)
+        return false;
+
+    ChannelName = ChannelNameNode.value;
+    ChannelName = ChannelName.trim();
+    if (ChannelName === "") {
+        UsrErrMsgNode.innerHTML = `Channel name cannot be left empty!!!`;
+        UsrErrMsgNode.style.display = "block";
+        return false;
+    }
+
+    ChannelGenre = ChannelGenreNode.value;
+    ChannelGenre = ChannelGenre.trim();
+    if (ChannelGenre === "") {
+        UsrErrMsgNode.innerHTML = `Channel genre cannot be left empty!!!`;
+        UsrErrMsgNode.style.display = "block";
+        return false;
+    }
+
+
+    const request = new XMLHttpRequest();
+    request.open('POST', '/AddChannel');
+
+    // Callback function for when request completes
+    request.onload = () => {
+
+        // Extract JSON data from request
+        const data = JSON.parse(request.responseText);
+        let UsrErrMsgNode = document.querySelector('#UserErrMsg') ; 
+        ChannelNameNode = document.querySelector('#inputChannelName');
+        ChannelGenreNode = document.querySelector('#inputChannelGenre');
+    
+
+        // Update the result div
+        if (data.success) 
+        {
+            if ( UsrErrMsgNode)
+                UsrErrMsgNode.innerHTML = "";
+            if ( ChannelNameNode )
+                ChannelNameNode.value ="" ;
+            if ( ChannelGenreNode )
+                ChannelGenreNode.value ="" ;
+        }
+        else 
+        {
+            if ( UsrErrMsgNode)
+            {
+                if ( data.reason )
+                    UsrErrMsgNode.innerHTML = data.reason ;
+                else 
+                    UsrErrMsgNode.innerHTML = "Unknowm reason" ;
+                UsrErrMsgNode.style.display = "block";
+            }
+        }
+    }
+
+    // Add data to send with request
+    const data = new FormData();
+    data.append('ChannelName', ChannelName);
+    data.append('ChannelGenre', ChannelGenre);
+
+    // Send request
+    request.send(data);
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    let UsrErrMsgNode = document.querySelector('#UserErrMsg') ; 
+    if ( UsrErrMsgNode && UsrErrMsgNode.innerHTML === "" )
+        UsrErrMsgNode.style.display = "none";
+
+});
